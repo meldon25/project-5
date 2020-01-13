045e59ca0d29ee4129ba766e86a2ec5057fdec48
Melissa Donegan <melissadonegan25@gmail.com>
2020-01-13 15:41:57 -0500

    auth working and react built

---
+++ project5_app_api/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :products
+  resources :users do 
+    collection do
+      post "/login", to: "users#login"
+      post "/register", to: "users#register"
+    end
+  end
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
